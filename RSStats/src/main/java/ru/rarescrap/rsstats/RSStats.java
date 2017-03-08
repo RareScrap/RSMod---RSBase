dd2ba251113098535f4bf42301993b132150c618
Писарев Юрий <rarescrap@gmail.com>
2017-03-08 11:17:53 +0800

    Обновлен StatItem (конструктор, addInformation) и файлы локализации

--- RSStats/src/main/java/ru/rarescrap/rsstats/RSStats.java
+++ RSStats/src/main/java/ru/rarescrap/rsstats/RSStats.java
@@ -20,10 +20,24 @@ public class RSStats {
     public static final String MODNAME = "RSStats"; // Имя мода
     public static final String VERSION = "0.0.1a"; // Версия мода
     public static Item strenghtStatItem;
+    
+    public String[] dices = new String[10];
+    
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
-        strenghtStatItem = new StatItem();
+        this.dices[0] = "d4";
+        this.dices[1] = "d4+1";
+        this.dices[2] = "d6";
+        this.dices[3] = "d6+1";
+        this.dices[4] = "d8";
+        this.dices[5] = "d8+1";
+        this.dices[6] = "d10";
+        this.dices[7] = "d10+1";
+        this.dices[8] = "d12";
+        this.dices[9] = "d20";
+        
+        strenghtStatItem = new StatItem(dices, "StrenghtStatItem", "rarescrap:StrenghtIcon_", "item.StrenghtStatItem");
         GameRegistry.registerItem((Item)strenghtStatItem, (String)"StrenghtStatItem");
     }
 
--- RSStats/src/main/java/ru/rarescrap/rsstats/items/StatItem.java
+++ RSStats/src/main/java/ru/rarescrap/rsstats/items/StatItem.java
@@ -3,14 +3,16 @@ package ru.rarescrap.rsstats.items;
 import net.minecraft.client.gui.GuiScreen; // Нужен для зажима Shift при показе описания
 import net.minecraft.client.renderer.texture.IIconRegister; // Регистрация иконок в игре
 import net.minecraft.creativetab.CreativeTabs; // Вкладки креативного режима. Нужно для расположения в них элементов
-import net.minecraft.entity.player.EntityPlayer; // Сущность игрока. Передается в некоторые методы
-import net.minecraft.item.Item; // Родительский класс
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.IIcon;
-import net.minecraft.util.StatCollector; // Класс для получения строкиз файла локализации
 
+// Эти классы передаются в методы
+import net.minecraft.entity.player.EntityPlayer; // Сущность игрока.
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item; // Родительский класс
+
+import net.minecraft.util.IIcon; // Хранилище иконок
+import net.minecraft.util.StatCollector; // Класс для получения строкиз файла локализации
+import ru.rarescrap.rsstats.utils.DescriptionCutter; // Для обрезки длинных предложений
 import java.util.List;
-import ru.rarescrap.rsstats.utils.DescriptionCutter;
 
 public class StatItem extends Item {
     private int NUMBER_OF_SUBTYPES = 10; // Количество видов этого предмета (например, количество вида одной статы - от 1 до 10)
@@ -18,51 +20,85 @@ public class StatItem extends Item {
     public IIcon[] icons = new IIcon[NUMBER_OF_SUBTYPES]; // хранилище иконок для всего семейства предмета
     public String[] dices = new String[NUMBER_OF_SUBTYPES]; // Бросок, соответствующей каждому предмету из семейству
     private String descriprion; // Описание предмета, получаемое из файла локализации
-
-    public StatItem() {
-        this.dices[0] = "d4";
-        this.dices[1] = "d4+1";
-        this.dices[2] = "d6";
-        this.dices[3] = "d6+1";
-        this.dices[4] = "d8";
-        this.dices[5] = "d8+1";
-        this.dices[6] = "d10";
-        this.dices[7] = "d10+1";
-        this.dices[8] = "d12";
-        this.dices[9] = "d20";
-        this.setUnlocalizedName("StrenghtStatItem");
+    private String registerIconPrefix; // "rarescrap:StrenghtIcon_" например
+    private String localePrefix; // "item.StrenghtStatItem" например
+    
+    public StatItem(String diceInput[], String unlocalizedName, String registerIconPrefix, String localePrefix) {
+        // TODO: Дайсы должны задаваться через серверный конфиг
+        for (int i = 0; i < NUMBER_OF_SUBTYPES; i++) { // Копирование входящих дайсов в поле класса
+            this.dices[i] = diceInput[i];
+        }
+        
+        this.localePrefix = localePrefix;
+        this.registerIconPrefix = registerIconPrefix;
+        
+        this.setUnlocalizedName(unlocalizedName);
         this.setMaxStackSize(1);
         this.setCreativeTab(CreativeTabs.tabMaterials);
         this.setHasSubtypes(true);
     }
 
+    @Override
     public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean par4) {
-        DescriptionCutter descriptionCutter = new DescriptionCutter();
-        list.add(this.dices[3]);
+        list.add( StatCollector.translateToLocal(localePrefix + ".name") );
+        
+        switch(itemstack.getItemDamage()) {
+        case 0:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 1) );
+            break;
+        case 1:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 2) );
+            break;
+        case 2:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 3) );
+            break;
+        case 3:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 4) );
+            break;
+        case 4:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 5) );
+            break;
+        case 5:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 6) );
+            break;
+        case 6:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 7) );
+            break;
+        case 7:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 8) );
+            break;
+        case 8:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 9) );
+            break;
+        case 9:
+            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 10) );
+            break;
+        }
+        
         list.add("");
         if (GuiScreen.isShiftKeyDown()) {
-            String[] descrStrings = descriptionCutter.cut(3, StatCollector.translateToLocal((String)"item.StatItem.descriprion"));
+            /*String[] descrStrings = descriptionCutter.cut(3, StatCollector.translateToLocal((String)"item.StatItem.descriprion"));
             for (int i = 0; i < descrStrings.length; ++i) {
                 list.add(descrStrings[i]);
-            }
+            }*/
+            list.add( StatCollector.translateToLocal(localePrefix + ".descriprion") );
         } else {
-            String[] moreInfoStrings = descriptionCutter.cut(3, StatCollector.translateToLocal((String)"item.StatItem.moreInformation"));
+            /*String[] moreInfoStrings = descriptionCutter.cut(3, StatCollector.translateToLocal((String)"item.StatItem.moreInformation"));
             for (int i = 0; i < moreInfoStrings.length; ++i) {
                 list.add(moreInfoStrings[i]);
-            }
+            }*/
+            list.add( StatCollector.translateToLocal(localePrefix + ".moreInfo") );
         }
     }
 
-    /**
-     * 
-     * @param reg 
-     */
+    @Override
     public void registerIcons(IIconRegister reg) {
         for (int i = 0; i < this.icons.length; ++i) {
-            this.icons[i] = reg.registerIcon("rsstats:strenght" + (i + 1));
+            this.icons[i] = reg.registerIcon(registerIconPrefix + (i + 1));
         }
     }
 
+    @Override
     public IIcon getIconFromDamage(int meta) {
         if (meta > this.icons.length) {
             meta = 0;
@@ -70,13 +106,16 @@ public class StatItem extends Item {
         return this.icons[meta];
     }
 
+    @Override
     public void getSubItems(Item item, CreativeTabs tab, List list) {
         for (int i = 0; i < this.icons.length; ++i) {
             list.add(new ItemStack(item, 1, i));
         }
     }
 
-    public String func_77667_c(ItemStack stack) {
-        return this.getUnlocalizedName() + "_" + (Integer.valueOf(stack.getItemDamage()) + 1);
+    @Override
+    public String getUnlocalizedName(ItemStack stack) {
+        return this.getUnlocalizedName();
+        //return this.getUnlocalizedName() + "_" + (Integer.valueOf( stack.getItemDamage() ) + 1); - пригодится когда каждому подтипу нужно дать индивидуальное имя
     }
 }
