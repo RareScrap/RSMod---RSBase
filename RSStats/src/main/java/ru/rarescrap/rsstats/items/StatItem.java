3aec5044a28d84107a2d55d75cc6503152e83788
Писарев Юрий <rarescrap@gmail.com>
2017-03-08 12:43:47 +0800

    Набросок кликов + локализации, которые кажись могут удалиться

--- RSStats/src/main/java/ru/rarescrap/rsstats/items/StatItem.java
+++ RSStats/src/main/java/ru/rarescrap/rsstats/items/StatItem.java
@@ -13,6 +13,9 @@ import net.minecraft.util.IIcon; // Хранилище иконок
 import net.minecraft.util.StatCollector; // Класс для получения строкиз файла локализации
 import ru.rarescrap.rsstats.utils.DescriptionCutter; // Для обрезки длинных предложений
 import java.util.List;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
 
 public class StatItem extends Item {
     private int NUMBER_OF_SUBTYPES = 10; // Количество видов этого предмета (например, количество вида одной статы - от 1 до 10)
@@ -40,42 +43,52 @@ public class StatItem extends Item {
 
     @Override
     public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean par4) {
-        list.add( StatCollector.translateToLocal(localePrefix + ".name") );
-        
+        // Строка броска (пример: "Бросок: d6+1")
         switch(itemstack.getItemDamage()) {
         case 0:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 1) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 1) );
             break;
         case 1:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 2) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 2) );
             break;
         case 2:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 3) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 3) );
             break;
         case 3:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 4) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 4) );
             break;
         case 4:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 5) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 5) );
             break;
         case 5:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 6) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 6) );
             break;
         case 6:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 7) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 7) );
             break;
         case 7:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 8) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 8) );
             break;
         case 8:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 9) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 9) );
             break;
         case 9:
-            list.add( StatCollector.translateToLocalFormatted(localePrefix + ".level", 10) );
+            list.add(StatCollector.translateToLocal(localePrefix + ".roll") + ": " +
+                    StatCollector.translateToLocalFormatted(localePrefix + ".level", 10) );
             break;
         }
+        list.add(""); // Пустая строка-разделитель
         
-        list.add("");
+        // Дополнительная информация по кнопке Shift
         if (GuiScreen.isShiftKeyDown()) {
             /*String[] descrStrings = descriptionCutter.cut(3, StatCollector.translateToLocal((String)"item.StatItem.descriprion"));
             for (int i = 0; i < descrStrings.length; ++i) {
@@ -118,4 +131,20 @@ public class StatItem extends Item {
         return this.getUnlocalizedName();
         //return this.getUnlocalizedName() + "_" + (Integer.valueOf( stack.getItemDamage() ) + 1); - пригодится когда каждому подтипу нужно дать индивидуальное имя
     }
+    
+    /*@Override
+    public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
+        entityplayer.addChatComponentMessage(new ChatComponentText("ROLL!"));
+        
+       return itemstack;
+        //return super.onItemRightClick(itemstack, world, entityplayer); //To change body of generated methods, choose Tools | Templates.
+    }*/
+    
+    
+    /*public float getStrVsBlock(ItemStack stack, Block block, int meta) 
+    {
+        entityplayer.addChatMessage("Open inventory");
+        
+        return (float) 1.0;
+    }*/
 }
