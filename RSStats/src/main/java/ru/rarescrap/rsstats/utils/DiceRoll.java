491518c6f764e6e47fabc4ba32f418f1a673df33
RareScrap <RareScrap@users.noreply.github.com>
2017-08-27 10:38:01 +0800

    Обьект броска сделан сериализированным

--- RSStats/src/main/java/ru/rarescrap/rsstats/utils/DiceRoll.java
+++ RSStats/src/main/java/ru/rarescrap/rsstats/utils/DiceRoll.java
@@ -5,21 +5,36 @@
  */
 package ru.rarescrap.rsstats.utils;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
- *
- * @author rares
+ * Объект броска дайсов
+ * @author RareScrap
  */
-public class DiceRoll {
-    public int dice;
+public class DiceRoll implements Serializable {
+    /** Количество граней на дайсе */
+    private int dice;
+    /** Список модификаторов, которые должны быть учтены */
     private List<RollModificator> modificators;
     
+    /**
+     * Конструктор, инициализирующий свои поля
+     * @param dice Количество граней на дайсе
+     */
     public DiceRoll(int dice) {
         this.dice = dice;
     }
     
+    /**
+     * Добавляет новый модификатор к броску
+     * @param rollModificator 
+     */
     public void addModificatot(RollModificator rollModificator) {
         modificators.add(rollModificator);
+    }
+    
+    public List<RollModificator> getModificators() {
+        return modificators;
     }
 }
