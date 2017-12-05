dd1edd8637bc1de63e5aabcb9ceadec88cc26f95
RareScrap <RareScrap@users.noreply.github.com>
2017-12-05 13:50:30 +0800

    Реализована отрисовка превью игрока на SSP UI

--- RSBase/src/main/java/rsstats/client/gui/MainMenuGUI.java
+++ RSBase/src/main/java/rsstats/client/gui/MainMenuGUI.java
@@ -6,6 +6,8 @@
 package rsstats.client.gui;
 
 import net.minecraft.client.renderer.InventoryEffectRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
@@ -13,6 +15,9 @@ import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import rsstats.common.RSStats;
 import net.minecraft.client.renderer.Tessellator;  //!!!
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
+import org.lwjgl.opengl.GL12;
 import rsstats.inventory.container.StatsContainer;
 
 /**
@@ -106,6 +111,53 @@ public class MainMenuGUI extends InventoryEffectRenderer {
         }*/
         
         //drawPlayerModel(k + 51, l + 75, 30, (float)(k + 51) - this.xSizeFloat, (float)(l + 75 - 50) - this.ySizeFloat, this.mc.thePlayer);
+        
+        // Орисовываем превью игрока
+        drawPlayerModel(k+15, l+40, 17, (float)(k + 51) - this.xSizeFloat, (float)(l + 75 - 50) - this.ySizeFloat, this.mc.thePlayer);
+    }
     
+    /**
+     * Отрисовывает превью игрока
+     * @param x TODO
+     * @param y TODO
+     * @param scale Маштаб модели
+     * @param yaw TODO
+     * @param pitch TODO
+     * @param playerdrawn TODO
+     */
+    public static void drawPlayerModel(int x, int y, int scale, float yaw, float pitch, EntityLivingBase playerdrawn) {
+        GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float)x, (float)y, 50.0F);
+        GL11.glScalef((float)(-scale), (float)scale, (float)scale);
+        GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+        float f2 = playerdrawn.renderYawOffset;
+        float f3 = playerdrawn.rotationYaw;
+        float f4 = playerdrawn.rotationPitch;
+        float f5 = playerdrawn.prevRotationYawHead;
+        float f6 = playerdrawn.rotationYawHead;
+        GL11.glRotatef(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GL11.glRotatef(-135.0F, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(-((float)Math.atan((double)(pitch / 40.0F))) * 20.0F, 1.0F, 0.0F, 0.0F);
+        playerdrawn.renderYawOffset = (float)Math.atan((double)(yaw / 40.0F)) * 20.0F;
+        playerdrawn.rotationYaw = (float)Math.atan((double)(yaw / 40.0F)) * 40.0F;
+        playerdrawn.rotationPitch = -((float)Math.atan((double)(pitch / 40.0F))) * 20.0F;
+        playerdrawn.rotationYawHead = playerdrawn.rotationYaw;
+        playerdrawn.prevRotationYawHead = playerdrawn.rotationYaw;
+        GL11.glTranslatef(0.0F, playerdrawn.yOffset, 0.0F);
+        RenderManager.instance.playerViewY = 180.0F;
+        RenderManager.instance.renderEntityWithPosYaw(playerdrawn, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+        playerdrawn.renderYawOffset = f2;
+        playerdrawn.rotationYaw = f3;
+        playerdrawn.rotationPitch = f4;
+        playerdrawn.prevRotationYawHead = f5;
+        playerdrawn.rotationYawHead = f6;
+        GL11.glPopMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
 }
